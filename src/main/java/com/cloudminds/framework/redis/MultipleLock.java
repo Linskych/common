e7cd37cfd4d457b25c95a6e6eb3157838f7df721
Skych Lin <skych.lin@cloudminds.com>
2020-05-14 19:21:40 +0800

    comment

--- src/main/java/com/cloudminds/framework/redis/MultipleLock.java
+++ src/main/java/com/cloudminds/framework/redis/MultipleLock.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
+/**
+ * Lock multiple keys at the same time. For example, user buys different products so you need to lock product codes at the same time.
+ * */
 public class MultipleLock {
 
     private static final Logger log = LoggerFactory.getLogger(MultipleLock.class);
