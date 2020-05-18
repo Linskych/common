3dd0d72efc1f488e6561d9b1d6c7d599200a459a
Skych Lin <skych.lin@cloudminds.com>
2020-05-18 09:57:23 +0800

    code style

--- src/main/java/com/cloudminds/framework/i18n/I18nProperties.java
+++ src/main/java/com/cloudminds/framework/i18n/I18nProperties.java
@@ -6,22 +6,37 @@ import org.springframework.stereotype.Component;
 @Component
 public class I18nProperties {
 
+    //i18n base name join by ",", such as "i18n/common,i18n/app"
     @Value("${spring.messages.basename:messages}")
-    private String basename;//i18n base name join by ",", such as "i18n/common,i18n/app"
+    private String basename;
+
     @Value("${spring.messages.encoding:UTF-8}")
     private String encoding;
+
+    //use system locale if true when parse locale unsuccessfully, or use default lang if false and default lang set.
     @Value("${spring.messages.fallback-to-system-locale:false}")
-    private boolean fallbackToSystemLocale;//use system locale if true when parse locale unsuccessfully, or use default lang if false and default lang set.
+    private boolean fallbackToSystemLocale;
+
+    //use key as result if true when no related value found, or empty string if false
     @Value("${spring.messages.use-code-as-default-message:true}")
-    private boolean useCodeAsDefaultMessage;//use key as result if true when no related value found, or empty string if false
-    @Value("${spring.messages.defaultLang:en}")
-    private String defaultLang;//use this lang when parse locale unsuccessfully and fallbackToSystemLocale set as false
+    private boolean useCodeAsDefaultMessage;
+
+    //use this lang when parse locale unsuccessfully and fallbackToSystemLocale set as false
+    @Value("${spring.messages.lang-default:en}")
+    private String defaultLang;
+
+    //set lang into http session if true
     @Value("${spring.messages.use-session:false}")
-    private boolean useSession;//set lang into http session if true
+    private boolean useSession;
+
+    //the attribute name when set lang into http session
     @Value("${spring.messages.lang-attribute-name:i18n-lang}")
-    private String langAttributeName;//the attribute name when set lang into http session
+    private String langAttributeName;
+
+    //request.getParameter(langParamName)
     @Value("${spring.messages.lang-param-name:lang}")
-    private String langParamName;//request.getParameter(langParamName)
+    private String langParamName;
+
 
     public String getBasename() {
         return basename;
