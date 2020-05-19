a8715a73168843f7d102fbaf101b6b6bbe3c9033
Skych Lin <skych.lin@cloudminds.com>
2020-05-19 18:20:10 +0800

    configure log4j2(not finished)

--- src/main/java/com/cloudminds/framework/TestController.java
+++ src/main/java/com/cloudminds/framework/TestController.java
@@ -2,6 +2,8 @@ package com.cloudminds.framework;
 
 import com.cloudminds.framework.redis.StringRedisService;
 import com.cloudminds.framework.response.R;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    Logger log = LoggerFactory.getLogger(TestController.class);
+
     @Autowired
     StringRedisService stringRedisService;
 
     @GetMapping("/test/lang")
     public R test() {
-        stringRedisService.set("teslang", "en");
+        Integer.parseInt("kk");
+        log.info("/test/lang");
         return R.ok();
     }
 }
