f766a5a253467022fc1dc7ca265c7984e2b87a4b
Skych Lin <skych.bluedream@gamil.com>
2020-05-26 22:49:15 +0800

    mybatis

---
+++ src/main/java/com/cloudminds/framework/repo/mysql/MysqlConfig.java
@@ -0,0 +1,9 @@
+package com.cloudminds.framework.repo.mysql;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource("classpath:mysql.properties")
+@Configuration
+public class MysqlConfig {
+}
